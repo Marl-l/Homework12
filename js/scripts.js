850b29173747bf2cf6923ca649a99ed179ae4a43
Marl-l <marltalalaev@gmail.com>
2021-01-27 00:41:17 +0200

    commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$ (document).ready(function(){
+
+    var mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    });
+    
+});
